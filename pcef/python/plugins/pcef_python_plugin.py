ebd1c15d5b67ba2bd470512b9e68445c25dc988b
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2013-07-22 13:05:42 +0200

    Adapt plugin

--- pcef/python/plugins/pcef_python_plugin.py
+++ pcef/python/plugins/pcef_python_plugin.py
@@ -9,17 +9,24 @@ in your path on Windows)
 """
 # This only works with PyQt, PySide does not support the QtDesigner module
 import os
-os.environ.setdefault("QT_API", "PyQt")
+if not 'QT_API' in os.environ:
+    os.environ.setdefault("QT_API", "PyQt")
 import pcef.python
-from pcef.core.plugins.pcef_core_plugin import QCodeEditPlugin
 
+PLUGINS_TYPES = {'QPythonCodeEdit': pcef.python.QPythonCodeEdit}
 
-class QPythonCodeEditPlugin(QCodeEditPlugin):
-    _module = 'pcef.python'        # path to the widget's module
-    _class = 'QPythonCodeEdit'    # name of the widget class
-    _name = "QPythonCodeEdit"
-    _icon = None
-    _type = pcef.python.QPythonCodeEdit
+try:
+    from pcef.core.plugins.pcef_core_plugin import QCodeEditPlugin
 
-    def createWidget(self, parent):
-        return pcef.python.QPythonCodeEdit(parent)
+    class QPythonCodeEditPlugin(QCodeEditPlugin):
+        _module = 'pcef.python'        # path to the widget's module
+        _class = 'QPythonCodeEdit'    # name of the widget class
+        _name = "QPythonCodeEdit"
+        _icon = None
+        _type = pcef.python.QPythonCodeEdit
+
+        def createWidget(self, parent):
+            return pcef.python.QPythonCodeEdit(parent)
+except ImportError:
+    print("Cannot use pcef plugins without PyQt4")
+
