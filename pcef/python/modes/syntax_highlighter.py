d0b28847d75d3c941cf302021f15e48bb8e093a9
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2013-07-28 14:44:13 +0200

    Add missing license headers

--- pcef/python/modes/syntax_highlighter.py
+++ pcef/python/modes/syntax_highlighter.py
@@ -1,4 +1,16 @@
-# syntax.py
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# PCEF - Python/Qt Code Editing Framework
+# Copyright 2013, Colin Duquesnoy <colin.duquesnoy@gmail.com>
+#
+# This software is released under the LGPLv3 license.
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+"""
+This module contains the python specific syntax highlighter
+"""
 from pcef.core import TextStyle
 from pcef.qt.QtCore import QRegExp
 from pcef.qt.QtGui import QColor, QTextCharFormat, QFont, QSyntaxHighlighter
