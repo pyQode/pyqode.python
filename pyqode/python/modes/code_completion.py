37f5f1feffcb3501254362829e8b3975638bc4a4
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2013-08-25 23:41:29 +0200

    Updated to use jedi 0.7

--- pyqode/python/modes/code_completion.py
+++ pyqode/python/modes/code_completion.py
@@ -27,8 +27,6 @@ import sys
 
 from pyqode.core import CompletionProvider, logger
 from pyqode.core import Completion
-from pyqode.core import indexByName
-from pyqode.core import logger
 
 
 ICONS = {'CLASS': ':/pyqode_python_icons/rc/class.png',
@@ -36,6 +34,7 @@ ICONS = {'CLASS': ':/pyqode_python_icons/rc/class.png',
          'STATEMENT': ':/pyqode_python_icons/rc/var.png',
          'FORFLOW': ':/pyqode_python_icons/rc/var.png',
          'MODULE': ':/pyqode_python_icons/rc/keyword.png',
+         'KEYWORD': ':/pyqode_python_icons/rc/keyword.png',
          'PARAM': ':/pyqode_python_icons/rc/var.png',
          'PARAM-PRIV': ':/pyqode_python_icons/rc/var.png',
          'PARAM-PROT': ':/pyqode_python_icons/rc/var.png',
@@ -56,37 +55,11 @@ class JediCompletionProvider(CompletionProvider):
             if self.addToPath:
                 dir = os.path.dirname(filePath)
                 sys.path.append(dir)
-            names = jedi.api.defined_names(code, filePath, fileEncoding)
-            index = indexByName(names, "QtCore")
-            if index != -1:
-                names.insert(0, names.pop(index))
-            index = indexByName(names, "QtGui")
-            if index != -1:
-                names.insert(0, names.pop(index))
-            index = indexByName(names, "numpy")
-            if index != -1:
-                names.insert(0, names.pop(index))
-            toPreLoad = []
-            for definition in names:
-                script = ""
-                if definition.type == "import":
-                    script = "{0};{1}.".format(definition.description, definition.name)
-                elif definition.type == "class":
-                    script = "{0}.".format(definition.name)
-                if script:
-                    toPreLoad.append((definition, script))
-            nb = len(toPreLoad)
-            for i, elem in enumerate(toPreLoad):
-                definition = elem[0]
-                script = elem[1]
-                msg = "Parsing {2} ({0}/{1})".format(i+1, nb, definition.name)
-                try:
-                    jedi.Script(script, 1, len(script), "").completions()
-                except Exception as e:
-                    logger.error("Failed to parse %s - %s" % (definition.name, e))
+            fn = os.path.splitext(os.path.basename(filePath))[0]
+            jedi.api.preload_module(fn)
         except Exception as e:
             logger.error("JediCompletionProvider failed to preload: %s" % e)
-        return CompletionProvider.preload(self, code, fileEncoding, filePath)
+        return None
 
     def complete(self, code, line, column, completionPrefix,
             filePath, fileEncoding):
@@ -94,30 +67,29 @@ class JediCompletionProvider(CompletionProvider):
             retVal = []
             script = jedi.Script(code, line, column,
                                  "", fileEncoding)
-            # print("Jedi run", line, column)
             completions = script.completions()
-            # print(len(completions))
             for completion in completions:
-                    # get type from description
-                    desc = completion.description
-                    suggestionType = desc.split(':')[0].upper()
-                    # get the associated icon if any
-                    icon = None
-                    if (suggestionType == "FORFLOW" or
-                            suggestionType == "STATEMENT"):
-                        suggestionType = "PARAM"
-                    if suggestionType == "PARAM" or suggestionType == "FUNCTION":
-                        if completion.name.startswith("__"):
-                            suggestionType += "-PRIV"
-                        elif completion.name.startswith("_"):
-                            suggestionType += "-PROT"
-                    if suggestionType in ICONS:
-                        icon = ICONS[suggestionType]
-                    else:
-                        logger.warning("Unimplemented completion type: %s" %
-                                       suggestionType)
-                    retVal.append(Completion(completion.name, icon=icon,
-                                             tooltip=desc.split(':')[1]))
+                # get type from description
+                desc = completion.description
+                suggestionType = desc.split(':')[0].upper()
+                # get the associated icon if any
+                icon = None
+                if (suggestionType == "FORFLOW" or
+                        suggestionType == "STATEMENT"):
+                    suggestionType = "PARAM"
+                if suggestionType == "PARAM" or suggestionType == "FUNCTION":
+                    if completion.name.startswith("__"):
+                        suggestionType += "-PRIV"
+                    elif completion.name.startswith("_"):
+                        suggestionType += "-PROT"
+                # print(completion, desc)
+                if suggestionType in ICONS:
+                    icon = ICONS[suggestionType]
+                else:
+                    logger.warning("Unimplemented completion type: %s" %
+                                   suggestionType)
+                retVal.append(Completion(completion.name, icon=icon,
+                                         tooltip=desc.split(':')[1]))
         except Exception as e:
             logger.error("Jedi failed to provide completions. Error: %s" % e)
         return retVal
