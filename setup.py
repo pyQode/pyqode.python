efa6642f9d8f13cd056f6c5dff1358dd45b07023
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2013-10-19 20:03:31 +0200

    Add ez_setup

--- setup.py
+++ setup.py
@@ -26,7 +26,12 @@
 """
 This setup script packages pyqode.python
 """
-from setuptools import setup, find_packages
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    import ez_setup
+    ez_setup.use_setuptools()
+    from setuptools import setup, find_packages
 
 
 def read_version():
@@ -50,17 +55,31 @@ setup(
     namespace_packages=['pyqode'],
     version=read_version(),
     packages=find_packages(),
-    keywords=["QCodeEditor", "PySide", "PyQt", "code editor", "python"],
+    keywords=["QCodeEditor PySide PyQt code editor widget python"],
     package_data={'pyqode.python.ui': ['*.ui', 'rc/*']},
     package_dir={'pyqode': 'pyqode'},
     url='https://github.com/ColinDuquesnoy/pyqode.python',
     license='MIT',
     author='Colin Duquesnoy',
     author_email='colin.duquesnoy@gmail.com',
-    description='Python/Qt Code Editor widget',
+    description='Add python support to pyqode',
     long_description=readme(),
     install_requires=requirements,
     entry_points={'pyqode_plugins':
                  ['pyqode_python = pyqode.python.plugins.pyqode_python_plugin']},
-    zip_safe=False
+    zip_safe=False,
+    classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: X11 Applications :: Qt',
+          'Environment :: Win32 (MS Windows)',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: Microsoft :: Windows',
+          'Operating System :: POSIX :: Linux',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
+          'Topic :: Software Development :: Libraries :: Application Frameworks',
+          'Topic :: Software Development :: Widget Sets',
+          'Topic :: Text Editors :: Integrated Development Environments (IDE)']
 )
