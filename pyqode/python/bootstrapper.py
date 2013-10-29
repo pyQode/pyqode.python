438020d946d9465e0473ba5bfc4fb0a1d0c1c0d6
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2013-10-29 20:34:08 +0100

    #9 Add missing license headers + log bootstrap time (debug log)

--- pyqode/python/bootstrapper.py
+++ pyqode/python/bootstrapper.py
@@ -1,3 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#The MIT License (MIT)
+#
+#Copyright (c) <2013> <Colin Duquesnoy and others, see AUTHORS.txt>
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in
+#all copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#THE SOFTWARE.
+#
+import time
 from pyqode.core import CodeCompletionMode, logger
 from pyqode.qt import QtCore
 
@@ -39,7 +65,9 @@ class Bootstrapper(QtCore.QObject):
         server.signals.workCompleted.connect(self._onWorkFinished)
         if self.modules:
             server.requestWork(self, PreloadWorker(self.modules))
+            self._start = time.time()
 
     def _onWorkFinished(self, caller_id, worker):
         if caller_id == id(self) and isinstance(worker, PreloadWorker):
             self.preLoadFinished.emit()
+            logger.debug("Bootstrapped in %f [s]" % (time.time() - self._start))
