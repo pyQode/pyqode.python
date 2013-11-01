891e4a9e589713a8cdb4f8a5f4be634c327df504
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2013-11-01 19:19:56 +0100

    #18 Move worker to the document analyser

--- pyqode/python/panels/symbol_browser.py
+++ pyqode/python/panels/symbol_browser.py
@@ -2,54 +2,9 @@
 SymbolBrowserPanel
 """
 import pyqode.core
-from pyqode.core import logger
-from pyqode.python.modes.code_completion import iconFromType
 from pyqode.qt import QtGui
 
 
-class Definition(object):
-    def __init__(self, name, icon, line, column):
-        self.icon = icon
-        self.name = name
-        self.line = line
-        self.column = column
-        # symbol name with parent name (for function and class variables)
-        self.full_name = ""
-        self.children = []
-
-    def add_child(self, definition):
-        definition.full_name = "%s.%s" % (self.name, definition.name)
-        self.children.append(definition)
-
-    def __repr__(self):
-        return 'Definition(%r, %r, %r, %r)' % (self.name, self.icon,
-                                               self.line, self.column)
-
-
-class DefinedNamesWorker(object):
-    def __init__(self, code, path, encoding):
-        self.code = code
-        self.path = path
-        self.encoding = encoding
-
-    def __call__(self, *args, **kwargs):
-        import jedi
-        ret_val = []
-        toplvl_definitions = jedi.defined_names(self.core, self.path, self.encoding)
-        for d in toplvl_definitions:
-            icon = ""
-            definition = Definition(d.name, iconFromType(d.type),
-                                    d.line, d.column)
-            if d.type == "class":
-                sub_definitions = d.defined_names()
-                for sub_d in sub_definitions:
-                    sub_definition = Definition(sub_d.name,
-                                                iconFromType(sub_d.type),
-                                                sub_d.line, sub_d.column)
-                    definition.add_child(sub_definition)
-            ret_val.append(definition)
-        logger.debug(ret_val)
-        return ret_val
 
 
 class SymbolBrowserPanel(pyqode.core.Panel):
