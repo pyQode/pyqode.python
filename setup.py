aa7402bd4e244ca701963b069e9b76934714ad36
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2014-02-25 12:00:36 +0100

    Bump version to 1.3.0-beta1
    and update requirements

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ def readme():
 
 
 # get requirements
-requirements = ['pyqode.core>=1.3.0-alpha1', 'jedi>=0.7', 'pep8', 'pyflakes']
+requirements = ['pyqode.core>=1.3.0-beta1', 'jedi>=0.7', 'pep8', 'pyflakes']
 
 
 setup(
