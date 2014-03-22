71dacf0598bc5ab818344bb1ad621c3ac5b92d5e
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2014-03-22 17:15:32 +0100

    Add a way to set sys path when starting the server

--- pyqode/python/server.py
+++ pyqode/python/server.py
@@ -24,17 +24,36 @@
 #THE SOFTWARE.
 #
 """
-Simple server which adds the code_completion.DocumentWordsProvider to the
-CodeCompletion worker.
+Server main script customised for pyqode.python.
+
+Users of pyqode.python can directly use this script. If you need to append
+directories to sys.path, use the '-s' command line arguments, e.g.::
+
+    python server.py 8080 -s /home/your_name/MyProj /home/your_name/MyLib
+
 """
+import sys
 from pyqode.core.api import code_completion
 from pyqode.core.api import server
 from pyqode.core.api import workers
-
 from pyqode.python import code_completion as py_code_completion
 
+
 if __name__ == '__main__':
+    # setup argument parser and parse command line args
+    parser = server.default_parser()
+    parser.add_argument('-s', '--syspath', nargs='*')
+    args = parser.parse_args()
+
+    # add user paths to sys.path
+    if args.syspath:
+        for path in args.syspath:
+            sys.path.insert(0, path)
+
+    # setup completion providers
     workers.CodeCompletion.providers.append(py_code_completion.JediProvider())
     workers.CodeCompletion.providers.append(
         code_completion.DocumentWordsProvider())
-    server.run()
+
+    # starts the server
+    server.run(args)
