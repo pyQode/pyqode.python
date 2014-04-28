bb82e3a8519009311ede80f877844565c49384b4
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2014-04-28 15:44:16 +0200

    Add a log file on windows

--- examples/qidle/qidle.py
+++ examples/qidle/qidle.py
@@ -1,8 +1,12 @@
 #! /usr/bin/python3
 # -*- coding: utf-8 -*-
+import sys
 from qidle import main
 import logging
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
+    filename = None
+    if sys.platform == 'win32':
+        filename = 'qidle.log'
+    logging.basicConfig(level=logging.INFO, filename=filename)
     main()
