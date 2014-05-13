3e58b23958e3fd9b84b82b48a81af6e66f2073f6
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2014-05-13 10:13:55 +0200

    Fix indenter mode when line is empty

--- pyqode/python/frontend/modes/indenter.py
+++ pyqode/python/frontend/modes/indenter.py
@@ -2,8 +2,9 @@
 """
 Contains the python indenter.
 """
-from pyqode.core.frontend.modes import IndenterMode
 from PyQt4 import QtGui
+from pyqode.core.frontend.modes import IndenterMode
+from pyqode.core import settings
 
 
 class PyIndenterMode(IndenterMode):
@@ -18,7 +19,18 @@ class PyIndenterMode(IndenterMode):
         assert isinstance(cursor, QtGui.QTextCursor)
         if not cursor.hasSelection():
             cursor.select(cursor.LineUnderCursor)
-        self.indent_selection(cursor)
+        if cursor.hasSelection():
+            self.indent_selection(cursor)
+        else:
+            # simply insert indentation at the cursor position
+            tab_len = settings.tab_length
+            cursor.beginEditBlock()
+            if settings.use_spaces_instead_of_tabs:
+                cursor.insertText(tab_len * " ")
+            else:
+                cursor.insertText('\t')
+            cursor.endEditBlock()
+            self.editor.setTextCursor(cursor)
 
     def unindent(self):
         cursor = self.editor.textCursor()
