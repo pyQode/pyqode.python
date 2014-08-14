548fa6380fc0f76708e414a1a3165dde13a663d9
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2014-08-14 14:52:58 +0200

    Fix backward incompatility. This module is deprecated and will be removed in version 2.2

---
+++ pyqode/python/code_edit.py
@@ -0,0 +1,10 @@
+"""
+Deprecated; the PyCodeEdit class has been moved into pyqode.python.widgets.
+
+This file will be removed in the next minor version (2.2).
+"""
+from .widgets.code_edit import PyCodeEdit
+
+__all__ = [
+    'PyCodeEdit'
+]
