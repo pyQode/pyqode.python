b8c41ce6652c0e3b99126c998663feb307febec6
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2014-08-15 17:28:42 +0200

    Improve test comments

--- test/test_modes/test_comments.py
+++ test/test_modes/test_comments.py
@@ -1,6 +1,7 @@
 """
 Test the autocomplete mode
 """
+from pyqode.qt.QtTest import QTest
 from pyqode.core.api import TextHelper
 from pyqode.python import modes as pymodes
 
@@ -18,30 +19,41 @@ def test_enabled(editor):
 
 def test_comment_single(editor):
     editor.clear()
-    editor.setPlainText('import os\n')
+    editor.setPlainText('import os')
+    QTest.qWait(100)
+    TextHelper(editor).goto_line(0, 0)
+    QTest.qWait(100)
     mode = get_mode(editor)
     mode.comment()
-    assert editor.toPlainText() == '# import os\n'
-    assert TextHelper(editor).current_line_nbr() == 1
+    QTest.qWait(100)
+    assert editor.toPlainText() == '# import os'
+    assert TextHelper(editor).current_line_nbr() == 0
 
 
 def test_uncomment_single(editor):
     editor.clear()
     editor.setPlainText('# import os\n')
+    QTest.qWait(100)
     mode = get_mode(editor)
+    QTest.qWait(100)
     mode.comment()
+    QTest.qWait(100)
     assert editor.toPlainText() == 'import os\n'
     editor.clear()
     editor.setPlainText('#import os\n')
+    QTest.qWait(100)
     mode = get_mode(editor)
     mode.comment()
+    QTest.qWait(100)
     assert editor.toPlainText() == '# #import os\n'
 
 
 def test_comment_selection(editor):
     editor.clear()
     editor.setPlainText('import os;\n  \ndef foo():\n    print("bar")')
+    QTest.qWait(100)
     mode = get_mode(editor)
     editor.selectAll()
     mode.comment()
+    QTest.qWait(100)
     assert editor.toPlainText() == '# import os;\n  \n# def foo():\n#     print("bar")'
