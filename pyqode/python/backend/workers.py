03a2bec9196bb1da12fa6c2d9959c24d1d091461
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2014-08-15 13:28:19 +0200

    Fix bugs highlighted by the new tests

--- pyqode/python/backend/workers.py
+++ pyqode/python/backend/workers.py
@@ -63,7 +63,8 @@ def goto_assignments(request_data):
     except jedi.NotFoundError:
         pass
     else:
-        ret_val = [(d.module_path, d.line, d.column, d.full_name)
+        ret_val = [(d.module_path, d.line - 1 if d.line else None,
+                    d.column, d.full_name)
                    for d in definitions]
         return True, ret_val
 
@@ -170,7 +171,7 @@ def defined_names(request_data):
         d_line, d_column = d.start_pos
         # use full name for import type
         definition = Definition(d.name, icon_from_typename(d.name, d.type),
-                                d_line, d_column, d.full_name)
+                                d_line - 1, d_column, d.full_name)
         # check for methods in class
         if d.type == "class" or d.type == 'function':
             try:
@@ -248,7 +249,7 @@ def run_pep8(request_data):
     else:
         messages = []
         for line_number, offset, code, text, doc in results:
-            messages.append(('[PEP8] %s' % text, WARNING, line_number))
+            messages.append(('[PEP8] %s' % text, WARNING, line_number - 1))
         return True, messages
 
 
@@ -275,7 +276,7 @@ def run_frosted(request_data):
                            _ast.PyCF_ONLY_AST)
         except SyntaxError as value:
             msg = '[pyFlakes] %s' % value.args[0]
-            (lineno, offset, text) = value.lineno, value.offset, value.text
+            (lineno, offset, text) = value.lineno - 1, value.offset, value.text
             # If there's an encoding problem with the file, the text is None
             if text is None:
                 # Avoid using msg, since for the only known case, it
@@ -292,7 +293,7 @@ def run_frosted(request_data):
             for warning in w.messages:
                 msg = "[pyFlakes] %s: %s" % (
                     warning.type.error_code, warning.message.split(':')[-1])
-                line = warning.lineno
+                line = warning.lineno - 1
                 status = (WARNING if warning.type.error_code.startswith('W')
                           else ERROR)
                 ret_val.append((msg, status, line))
