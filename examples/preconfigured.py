47f47ddbc204ff966c5e03030c37c585960d7a4a
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2014-11-11 12:24:27 +0100

    Add preconfigured editor example (PyCodeEdit)
    See pyQode/pyQode#11

---
+++ examples/preconfigured.py
@@ -0,0 +1,21 @@
+"""
+This simple example shows you how to use the pre-configured python editor.
+"""
+# optionally, set the qt api to use (in ['pyqt4', 'pyqt5', 'pyside'])
+import os; os.environ['QT_API'] = 'pyside'
+import sys
+from pyqode.qt import QtWidgets
+from pyqode.python.backend import server
+from pyqode.python.widgets import PyCodeEdit
+from pyqode.python.widgets import code_edit
+from pyqode.python.widgets import outline
+
+
+app = QtWidgets.QApplication(sys.argv)
+window = QtWidgets.QMainWindow()
+editor = PyCodeEdit(server_script=server.__file__)
+# show the PyCodeEdit module in the editor
+editor.file.open(code_edit.__file__.replace('.pyc', '.py'))
+window.setCentralWidget(editor)
+window.show()
+app.exec_()
