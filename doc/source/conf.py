c77571f988ee1f99465a221c3afd04683921a191
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2014-11-13 19:03:25 +0100

    Get rid of those unused mock objects

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -20,35 +20,8 @@ import sys, os
 sys.path.append(
     os.path.abspath(os.path.join(os.path.dirname(__file__), "_ext")))
 sys.path.insert(0, os.path.abspath('../..'))
-
-os.environ['SPHINX'] = "1"
-
 import sys
-
 from pyqode.python import __version__
-
-
-class Mock(object):
-    def __init__(self, *args, **kwargs):
-        pass
-
-    def __call__(self, *args, **kwargs):
-        return Mock()
-
-    @classmethod
-    def __getattr__(cls, name):
-        if name in ('__file__', '__path__'):
-            return '/dev/null'
-        elif name[0] == name[0].upper():
-            mockType = type(name, (), {})
-            mockType.__module__ = __name__
-            return mockType
-        else:
-            return Mock()
-
-MOCK_MODULES = ["PyQt4", 'PyQt5']
-for mod_name in MOCK_MODULES:
-    sys.modules[mod_name] = Mock()
 
 # -- General configuration -----------------------------------------------------
 
