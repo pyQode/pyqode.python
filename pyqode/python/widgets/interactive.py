f2663a672ec37ac3833c28589db507b8ae1b1f7b
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2015-06-29 13:52:58 +0200

    Simplify PyInteractiveConsole file regex
    and fix bug if exception occur in dist-packages (there was an issue with the previous regex which did not take '-' into account)

--- pyqode/python/widgets/interactive.py
+++ pyqode/python/widgets/interactive.py
@@ -30,9 +30,8 @@ class PyInteractiveConsole(InteractiveConsole):
         self.set_writer(self._write)
         self.setMouseTracking(True)
         self.PROG = QtCore.QRegExp(
-            r'\s*File "[a-zA-Z\/_\d:\\\.]*((.\.[a-zA-Z\/_\d:\\]*")|(")), '
-            r'line [0-9]*.*')
-        self.FILENAME_PROG = QtCore.QRegExp(r'"[a-zA-Z\/_\.\d:\\]*"')
+            r'\s*File ".*", line [0-9]*, in ')
+        self.FILENAME_PROG = QtCore.QRegExp(r'".*"')
         self.LINE_PROG = QtCore.QRegExp(r'line [0-9]*')
         self.setLineWrapMode(self.NoWrap)
         self._module_color = QtGui.QColor('blue')
