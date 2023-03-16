79f0ba00660e3ddd3adf46fa696f8346aa56b0b9
Eric Prestat <eric.prestat@gmail.com>
2023-03-16 15:42:46 +0000

    Remove ReturnWithArgsInsideGenerator since it has been removed in pyflakes 2.5 and it is not necessary with python >=3

--- pyqode/python/backend/workers.py
+++ pyqode/python/backend/workers.py
@@ -176,7 +176,6 @@ def run_pep8(request_data):
 
 PYFLAKES_ERROR_MESSAGES = [
     messages.DoctestSyntaxError,
-    messages.ReturnWithArgsInsideGenerator,
     messages.UndefinedExport,
     messages.UndefinedName,
     messages.UndefinedLocal
